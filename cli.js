20218acf81926fa4f69a531fd5fcb16225da0f82
Mathias H Steffensen <ms@driversnote.com>
2021-06-22 09:54:27 +0200

    finish CLI tool

--- cli.js
+++ cli.js
@@ -1,9 +1,7 @@
-import {Logger} from "./logger";
+import { spam } from "./spammer.js"
 import yargs from "yargs";
 
 (async () => {
-    const log = new Logger()
-
     const args = yargs(process.argv.slice(2))
         .usage('\nUsage: $0 --target [target url/urls] --reqs [requests per second] --secs [the number of seconds to run]\n')
         .usage('Example: $0 --target https://google.com --reqs 100 --secs 20')
@@ -13,4 +11,6 @@ import yargs from "yargs";
     const urls = args.target.split(",")
     const reqsPerSec = args.reqs || 20
     const secsToRun = args.secs || 10
+
+    await spam(urls, reqsPerSec, secsToRun)
 })()
